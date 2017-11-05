79397dd4867f36c527a33871412f6b3ae050f0b3
Dylan Pincus <lohikansun@gmail.com>
2017-11-05 16:35:16 -0500

    changed url

--- lib/spotify/player.ex
+++ lib/spotify/player.ex
@@ -5,13 +5,17 @@ defmodule Spotify.Player do
 
   alias Spotify.{Client, Album, Artist, Playlist, Track}
 
+  def play_url(params) do
+    "https://api.spotify.com/v1/me/player/devices" <> query_string(params)
+  end
+
   def play(conn, params) do
-    url = "https://api.spotify.com/v1/me/player/play "
+    url = play_url(params)
     conn |> Client.put(url) |> handle_response
   end
 
   def pause(conn, params) do
-    url = "https://api.spotify.com/v1/me/player/pause "
+    url = "https://api.spotify.com/v1/me/player/pause"
     conn |> Client.put(url) |> handle_response
   end
 
