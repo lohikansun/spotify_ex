5255e4b03f472f4f6254b6b4652aad2d56c9debe
Dylan Pincus <lohikansun@gmail.com>
2017-11-18 16:20:14 -0500

    typo

--- lib/spotify/player.ex
+++ lib/spotify/player.ex
@@ -10,7 +10,9 @@ defmodule Spotify.Player do
     url = "https://api.spotify.com/v1/me/player/play"
     body = ~s({"uris":
 ["spotify:track:4iV5W9uYEdYUVa79Axb7Rh", "spotify:track:1301WleyT98MSxVHPZCA6M"]})
-    conn |> Client.put(url, body) |> handle_response
+    response = conn |> Client.put(url, body)
+    IEx.pry
+    handle_response(response)
   end
 
 
