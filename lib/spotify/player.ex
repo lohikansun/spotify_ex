669116965f1a2e8414eebe1876b20a4574238394
Dylan Pincus <lohikansun@gmail.com>
2017-11-18 16:31:38 -0500

    wip

--- lib/spotify/player.ex
+++ lib/spotify/player.ex
@@ -8,7 +8,7 @@ defmodule Spotify.Player do
 
   def play(conn, params) do
     url = "https://api.spotify.com/v1/me/player/play"
-    body = HTTPoisson.encode(params)
+    body = Poison.encode!(params)
     response = conn |> Client.put(url, body)
     IEx.pry
     handle_response(response)
